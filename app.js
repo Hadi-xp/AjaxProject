064446bc2f53d4cd5f5f99aa54d6d3a20ad6d088
Hadixp <hadi.b.2002@gmail.com>
2024-12-31 15:55:26 +0330

    server error added

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ btnC.addEventListener('click',()=>{
 // this is for server error
 btnS.addEventListener('click',()=>{
     const xhr = new XMLHttpRequest();
-    xhr.open('GET','https://hadi-xp.github.io/serverError/');
+    xhr.open('GET','https://httpstat.us/500');
     xhr.onload = function(){
         if(xhr.status >= 500){
             console.error('server error');
